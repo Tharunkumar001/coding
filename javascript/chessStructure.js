0dd48936e81982eaf8c42e68defa28c73fdb4d28
Tharunkumar001 <tharung1411@gmail.com>
2021-02-20 21:38:53 +0530

    pattern

--- javascript/chessStructure.js
+++ javascript/chessStructure.js
@@ -1,23 +1,5 @@
-var saveVariable = "";
-var str = "#"
-for(let i=1; i<=8;i++){
-    if(i%2!=0){
-        for(let j=1;j<=8;j++){
-            if(j%2!=0){
-                saveVariable+=str;
-            }else{
-                saveVariable+=" ";
-            }   
-        }
-    }else{
-        for(let k=1;k<=8;k++){
-            if(k%2!=0){
-                saveVariable+=str;
-            }else{
-                saveVariable+=" ";
-            }
-
-        }
-    }
-   
+for(let i=0;i<4;i++){
+    console.log("#  ".repeat(4));
+    console.log("  #".repeat(4));
 }
+
--- javascript/looping a triangle.js
+++ javascript/looping a triangle.js
@@ -4,6 +4,5 @@ function triangle() {
 	for(i=1;i<7;i++){
     	console.log("\n","#".repeat(i))
        }
-  
 }
 triangle();
